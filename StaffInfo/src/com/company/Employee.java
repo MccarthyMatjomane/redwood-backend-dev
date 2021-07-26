ad5313d331cc04d3edfad1c7990d627c24a46c75
MccarthyMatjomane <thagamccarthy@gmail.comPhogolo@8>
2021-07-26 22:42:30 +0200

    i used array to store the employees information and use inheritance to call them in the Main class

--- StaffInfo/src/com/company/Employee.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class Employee {
-    String[] employeeNames = {"Robert","Sam","John"};
-    int[] yearJoined = {1994,2000,1999};
-    String[] employeeAddress = {"64C- WallsStreat","68D- WallsStreat","26B- WallsStreat"};
-    int[] salary;
-}
